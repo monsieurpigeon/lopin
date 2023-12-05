ba82648172c2d0b2114f2a71f28be26b99570d3a
Maxime PIGEON <maxpige@gmail.com>
2023-12-05 15:25:33 +0100

    add emoji to title

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { Button, Flex, Text } from "@chakra-ui/react";
 export default function Header() {
   return (
     <Flex justifyContent="space-between" alignItems="center" p={2}>
-      <Text fontSize="2xl">Lopin</Text>
+      <Text fontSize="2xl">🏡 Lopin</Text>
       <Button
         onClick={() => {
           console.log("connect");
