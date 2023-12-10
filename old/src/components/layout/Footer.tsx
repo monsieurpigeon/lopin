74c12a481bec826452ba0bb2cee8fb643ba57961
Maxime PIGEON <maxpige@gmail.com>
2023-12-10 23:10:56 +0100

    remove old

--- old/src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex } from "@chakra-ui/react";
-
-export function Footer() {
-  return (
-    <Flex justifyContent="center" p={2}>
-      <p>Lopin 2023 - contact@oseille.app</p>
-    </Flex>
-  );
-}
--- old/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: { port: 5174 },
-  plugins: [react()],
-});
