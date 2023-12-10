d30c6e9b20f40d956482920d87ff258365ccb377
Maxime PIGEON <maxpige@gmail.com>
2023-12-10 14:11:56 +0100

    remove old code

--- old/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex } from "@chakra-ui/react";
-
-export function Footer() {
-  return (
-    <Flex justifyContent="center">
-      <p>Lopin 2023 - contact@oseille.app</p>
-    </Flex>
-  );
-}
