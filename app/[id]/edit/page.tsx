2c7639f503eddc2b63f699d33ff5d2be279d27be
Maxime PIGEON <maxpige@gmail.com>
2023-12-14 01:02:53 +0100

    fix input value

--- app/[id]/edit/page.tsx
+++ app/[id]/edit/page.tsx
@@ -1,11 +1,14 @@
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
+import z from "zod";
 import { Farm } from "../../../utils/interfaces";
 import { updateFarmById } from "./actions";
 
 interface Props {
   params: { id: string };
 }
+
+const schema = z.object({ name: z.string() });
 
 export default async function Page({ params }: Props) {
   const cookieStore = cookies();
@@ -16,11 +19,16 @@ export default async function Page({ params }: Props) {
     .eq("id", params.id);
 
   const farm = farms?.[0] as Farm;
-  const updateFarm = updateFarmById.bind(null, farm.id, { name: "PROUT" });
+
+  async function onSubmit(request: FormData) {
+    "use server";
+    const name = (request.get("name") as string) || farm.name;
+    updateFarmById(farm.id, { name });
+  }
 
   return (
     <div>
-      <form action={updateFarm}>
+      <form action={onSubmit}>
         <input type="text" name="name" defaultValue={farm.name} />
         <button type="submit">Enregistrer</button>
       </form>
